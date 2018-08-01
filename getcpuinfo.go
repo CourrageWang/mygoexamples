aaed300f4ac9b3f65ec21b59c77fc7e1ae2a373b
yq wang <couragewang@126.com>
2018-08-01 18:52:04 +0800

    memory use info

--- getcpuinfo.go
+++ getcpuinfo.go
@@ -1,90 +1,58 @@
 package main
 
 import (
-
-
-
 	"fmt"
-
 	"log"
-
 	"os"
-
 	"runtime/pprof"
-
-	"flag"
 	"time"
+	"flag"
+	"runtime"
 )
-
-
-
-var (
-
-	//定义外部输入文件名字
-
-	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file.")
-
-
-
-
-)
-
-
+//https://cloud.tencent.com/developer/section/1144254
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
 
 func main() {
-
 	log.Println("begin")
-
 	flag.Parse()
 
-
-
-	if *cpuprofile != "" {
-
-		f, err := os.Create(*cpuprofile)
-
+		f, err := os.Create("cpuprofile.prof")
 		if err != nil {
-
-			log.Fatal(err)
-
+			log.Fatal("could not create CPU profile: ", err)
 		}
-
-
-
-		pprof.StartCPUProfile(f)
-
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
 		defer pprof.StopCPUProfile()
 
-	}
-
-
-
+	//heapProfile("cpu1.prof")
 	for i := 0; i < 30; i++ {
 
 		nums := fibonacci2(i)
-		heapProfile("cpu1.prof")
-
+		//heapProfile("cpu1.prof")
 		fmt.Println(nums)
-
 	}
-	heapProfile("cpu1.prof")
-	//cpuprofile()
+
+		f2, err2 := os.Create("mem.prof")
+		if err2 != nil {
+			log.Fatal("could not create memory profile: ", err)
+		}
+		runtime.GC() // get up-to-date statistics
+		if err := pprof.WriteHeapProfile(f); err != nil {
+			log.Fatal("could not write memory profile: ", err)
+		}
+		f2.Close()
 
 
 }
 
 //递归实现的斐波纳契数列
-
 func fibonacci2(num int) int {
-
 	if num < 2 {
-
 		return 1
-
 	}
-
 	return fibonacci2(num-1) + fibonacci2(num-2)
-
 }
 func cpuProfile() {
 	f, err := os.OpenFile("cpu.prof", os.O_RDWR|os.O_CREATE, 0644)
@@ -110,7 +78,8 @@ func heapProfile(path string) {
 	defer f.Close()
 
 	time.Sleep(30 * time.Second)
+	defer f.Close()
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
 
-	pprof.WriteHeapProfile(f)
-	fmt.Println("Heap Profile generated")
 }
